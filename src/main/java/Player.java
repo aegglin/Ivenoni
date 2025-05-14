fbd1ccc540d47ad93a8a6f3f32ac2b2d5275b0a6
Aiden Egglin <uefadude@gmail.com>
2025-05-13 21:38:56 -0400

    Create player class

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Player {
+
+    public BufferedImage image;
+
+    public Player() {
+        try {
+            image = ImageIO.read(new File("/res/player/player.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void loadImage() {
+
+    }
+
 }
